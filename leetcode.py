379bbcbd664b3bf269607539c8d2e29439224941
yliu483 <yucheng.liu@gatech.edu>
2014-07-23 14:52:43 -0400

    Insertion Sort List + Reverse Nodes in k-Group

--- leetcode.py
+++ leetcode.py
@@ -1,8 +1,77 @@
 __author__ = 'le0nh@rdt'
 
 
+# Insertion Sort List
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @return a ListNode
+    def insertionSortList(self, head):
+        if head == None or head.next == None: return head
+        dummy = ListNode(0)
+        dummy.next = head
+        curr = head
+        
+        while curr.next:
+            if curr.next.val >= curr.val:
+                curr = curr.next
+            else:
+                pre = dummy
+                while pre.next.val < curr.next.val:
+                    pre = pre.next
+                tmp = curr.next
+                curr.next = curr.next.next
+                tmp.next = pre.next
+                pre.next = tmp
+        
+        return dummy.next
 
 
+
+
+
+# Reverse Nodes in k-Group 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @param k, an integer
+    # @return a ListNode
+    def reverseKGroup(self, head, k):
+        if head == None: return head
+        dummy = ListNode(0); dummy.next = head; start = dummy
+        while start.next:
+            end = start
+            for i in xrange(k-1):
+                end = end.next
+                if end.next == None: return dummy.next
+            res = self.reverse(start.next, end.next)
+            start.next = res[0]
+            start = res[1]
+        
+        return dummy.next
+    
+    def reverse(self, start, end):
+        _dummy=ListNode(0); _dummy.next=start
+        
+        while _dummy.next!=end:
+            tmp=start.next
+            start.next=tmp.next
+            tmp.next=_dummy.next
+            _dummy.next=tmp
+        
+        return [end, start]
+        
+            
 
 
 
