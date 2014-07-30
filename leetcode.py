3d03198cbabbd162bd1233c40d5260d39532949a
yliu483 <yucheng.liu@gatech.edu>
2014-07-30 15:17:36 -0400

    ZigZag Conversion

--- leetcode.py
+++ leetcode.py
@@ -1,6 +1,21 @@
 __author__ = 'le0nh@rdt'
+# Zigzag Conversion
+class Solution:
+    # @return a string
+    def convert(self, s, nRows):
+        if nRows == 1: return s
+        tmp=['' for i in range(nRows)]
+        index = -1
+        step = 1
+        for i in xrange(len(s)):
+            index+=step
+            if index == nRows:
+                index -= 2; step = -1
+            elif index == -1:
+                index = 1; step = 1
 
-
+            tmp[index]+= s[i]
+        return  ''.join(tmp)
 
 
 
