6508519fa864d0e7f056a4bc0ee70714df4e9bed
yliu483 <yucheng.liu@gatech.edu>
2014-08-01 16:13:08 -0400

    Valid Number

--- leetcode.py
+++ leetcode.py
@@ -1,4 +1,46 @@
 __author__ = 'le0nh@rdt'
+# Valid Number
+class Solution:
+    # @param s, a string
+    # @return a boolean
+    def isNumber(self, s):
+        INVALID = 0;
+        SPACE = 1;
+        SIGN = 2;
+        DIGIT = 3;
+        DOT = 4;
+        EXPONENT = 5
+        transitionMatrix = [[-1, 0, 3, 1, 2, -1],  # 0 no input or just spaces
+                            [-1, 8, -1, 1, 4, 5],  # 1 input is digits
+                            [-1, -1, -1, 4, -1, -1],  # 2 no digits in front just Dot
+                            [-1, -1, -1, 1, 2, -1],  #3 sign
+                            [-1, 8, -1, 4, -1, 5],  #4 digits and dot in front
+                            [-1, -1, 6, 7, -1, -1],  #5 input 'e' or 'E'
+                            [-1, -1, -1, 7, -1, -1],  #6 after 'e' input sign
+                            [-1, 8, -1, 7, -1, -1],  #7 after 'e' input digits
+                            [-1, 8, -1, -1, -1, -1]]  # 8 after valid input input space
+        state = 0;
+        i = 0
+        while i < len(s):
+            inputType = INVALID
+            if s[i] == ' ':
+                inputType = SPACE
+            elif s[i] == '-' or s[i] == '+':
+                inputType = SIGN
+            elif s[i] in '1234567890':
+                inputType = DIGIT
+            elif s[i] == '.':
+                inputType = DOT
+            elif s[i] == 'e' or s[i] == 'E':
+                inputType = EXPONENT
+
+            state = transitionMatrix[state][inputType]
+            if state == -1:
+                return False
+            else:
+                i += 1
+
+        return state == 1 or state == 4 or state == 7 or state == 8
 
 
 # Valid Palindrome
