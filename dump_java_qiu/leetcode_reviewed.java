52f7e5ca93ad443caf1a28f9c020fa1df3d9637c
SunnyQiu <qiuhanjiao89@gmail.com>
2014-08-09 17:22:45 -0400

    Update 2 problems

--- dump_java_qiu/leetcode_reviewed.java
+++ dump_java_qiu/leetcode_reviewed.java
@@ -101,3 +101,73 @@ public class Solution {
         return result;
     }
 }
+
+// 5. Binary Tree Postorder Traversal
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> nodeStack = new Stack<TreeNode>();
+        Stack<TreeNode> postOrder = new Stack<TreeNode>();
+        nodeStack.push(root);
+        
+        while (!nodeStack.isEmpty()) {
+            TreeNode lastNode = nodeStack.pop();
+            postOrder.push(lastNode);
+            
+            if (lastNode.left != null) {
+                nodeStack.push(lastNode.left);
+            }
+            if (lastNode.right != null) {
+                nodeStack.push(lastNode.right);
+            }
+        }
+        
+        while(!postOrder.isEmpty()) {
+            result.add(postOrder.pop().val);
+        }
+        return result;
+    }
+}
+
+// 6. Binary Tree Level Order Traversal
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        
+        Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        nodeQueue.offer(root);
+        
+        while (!nodeQueue.isEmpty()) {
+            List<Integer> level = new ArrayList<Integer>();
+            int size = nodeQueue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curt = nodeQueue.poll();
+                level.add(curt.val);
+                
+                if (curt.left != null) {
+                    nodeQueue.offer(curt.left);
+                }
+                if (curt.right != null) {
+                    nodeQueue.offer(curt.right);
+                }
+            }
+            result.add(level);
+        }
+        return result;
+    }
+}
